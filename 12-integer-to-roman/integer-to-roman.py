fef6328d088e1574162066d8f29fe5151c02ddda
JOEL AMOS PHILIP <163115164+Aggushub@users.noreply.github.com>
2025-06-09 10:28:41 +0530

    Time: 6 ms (47.65%) | Memory: 17.7 MB (69.53%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,28 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman = {
+            'I':1,
+            'IV':4,
+            'V':5,
+            'IX':9,
+            'X':10,
+            'XL':40,
+            'L':50,
+            'XC':90,
+            'C':100,
+            'CD':400,
+            'D':500,
+            'CM':900,
+            'M':1000
+
+        }
+        res = []
+
+        for key,value in reversed(roman.items()):
+            while num > 0:
+                if value <= num:
+                    res.append(key)
+                    num -= value
+                else:
+                    break
+        return "".join(res)
