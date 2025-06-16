4d92e7af1d8de030e41b360bbe891718c1e29985
JOEL AMOS PHILIP <163115164+Aggushub@users.noreply.github.com>
2025-06-16 11:47:29 +0530

    Time: 94 ms (33.10%) | Memory: 17.8 MB (59.91%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.py
@@ -1,13 +1,10 @@
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
-        maxdiff = 0
+        maxdiff = -1
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]<nums[j]:
                     dif = nums[j] - nums[i]
                     if dif > maxdiff:
                         maxdiff = dif
-        if maxdiff>0:
-            return maxdiff
-        else:
-            return -1
+        return maxdiff
