8517be0351b50548f33151461660c88193db9b6c
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-07-14 22:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (45.08%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let hare = head;
+    let turtle = head;
+
+    while(hare && hare.next){
+        turtle = turtle.next;
+        hare = hare.next.next;
+    }
+    return turtle;
+};
