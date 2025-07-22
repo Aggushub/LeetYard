d5f270d2ea933b308927ce536cc4a955ef48ca60
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-07-22 19:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (45.74%) - LeetSync

---
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function(nums) {
+    let min = nums[0];
+    let max_diff = -1;
+    for (let i = 0; i<nums.length;i++){
+        if (nums[i]>min)
+        max_diff = Math.max(max_diff,nums[i]-min);
+        else 
+        min = nums[i];
+    }
+    return max_diff;
+    
+};
