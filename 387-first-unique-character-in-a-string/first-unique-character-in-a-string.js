3e52b3312eaa09b43b8d3a189bb5d5cfe7e886ec
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-07-22 18:05:35 +0530

    Time: 38 ms (30.01%) | Memory: 60.1 MB (45.74%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -0,0 +1,15 @@
+var firstUniqChar = function(s) {
+    let hash = {};
+    for (let i = 0;i < s.length;i++){
+        if (s[i] in hash)
+        hash[s[i]]++;
+        else
+        hash[s[i]] = 1;
+    }
+
+    for (let i = 0;i < s.length;i++){
+        if (hash[s[i]] === 1)
+        return i;
+    }
+    return -1;
+};
