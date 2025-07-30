a05308a219f4b155f0d136a3d9c4fcc8a1e8ffaa
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-07-30 17:55:24 +0530

    Time: 6 ms (15.67%) | Memory: 56.9 MB (22.61%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,32 @@
+var isValid = function(s) {
+    let stack = [];
+    let top = "";
+    let close = "";
+    for(let i = 0;i<s.length;i++){
+        if (stack.length === 0){
+            stack.push(s[i]);
+            top = s[i];
+            continue;
+        }
+        if (top === "{")
+            close = "}";
+        else if (top === "[")
+            close = "]";
+        else if (top == "(")
+            close = ")";
+        else
+        close = ""
+        if (close === s[i]){
+            stack.pop();
+            top = stack[stack.length-1];
+        }
+        else{
+            stack.push(s[i]);
+            top = s[i];
+    }
+    }
+    if (stack.length === 0)
+    return true;
+    else
+    return false;
+};
