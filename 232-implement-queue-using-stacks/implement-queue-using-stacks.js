4adf69b7112e6f8f3d06653adb086a1661501dff
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-08-05 13:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 53.4 MB (58.60%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.js
@@ -0,0 +1,37 @@
+var MyQueue = function() {
+    this.instack = [];
+    this.outstack = [];
+};
+
+
+MyQueue.prototype.push = function(x) {
+    this.instack.push(x);
+};
+
+
+MyQueue.prototype.pop = function() {
+    if (this.outstack.length === 0)
+    {
+        while(this.instack.length){
+            this.outstack.push(this.instack.pop());
+        }
+    }
+    return this.outstack.pop();
+};
+
+
+MyQueue.prototype.peek = function() {
+    if (this.outstack.length === 0)
+    {
+        while(this.instack.length){
+            this.outstack.push(this.instack.pop());
+        }
+    }
+    return this.outstack[this.outstack.length-1]; 
+    
+};
+
+
+MyQueue.prototype.empty = function() {
+    return this.instack.length === 0 && this.outstack.length === 0
+};
