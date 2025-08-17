46c0a904f24ccf8cfabcc9dccf9a65efd0a04e9e
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-08-17 18:12:54 +0530

    Time: 1 ms (90.71%) | Memory: 59.6 MB (69.54%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -1,18 +1,8 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var findMaxConsecutiveOnes = function(nums) {
-    let max = 0;
-    let cons = 0;
-    for(let dig of nums){
-        if (dig === 1){
-            cons++;
-            max = Math.max(cons,max);
-        }
-        else
-        cons = 0;
+    let max = 0, cons = 0;
+    for (let i = 0; i < nums.length; i++) {
+        cons = nums[i] === 1 ? cons + 1 : 0;
+        if (cons > max) max = cons;
     }
     return max;
-    
 };
