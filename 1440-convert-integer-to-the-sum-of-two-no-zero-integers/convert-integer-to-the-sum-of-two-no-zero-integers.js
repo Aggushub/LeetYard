7ee5924926cb186116d50ad390e1bf66e7e419f4
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-09-08 16:25:38 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (49.28%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function(n) {
+    for (let i= 0;i<n;i++){
+        if (String(i).includes('0'))
+        continue;
+    j = n - i;
+    if (String(j).includes('0'))
+    continue;
+    return [i,j];
+    }
+};
