fa6b38540f0966f9d0768016a52cf7cc7775e282
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-09-29 22:14:57 +0530

    Time: 65 ms (5.67%) | Memory: 55.2 MB (98.73%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.js
+++ 1580-shuffle-the-array/shuffle-the-array.js
@@ -2,9 +2,9 @@ var shuffle = function(nums, n) {
     let turtle = 0;
     let hare = n;
     let out = [];
-    while(turtle<n){
-        out.push(nums[turtle]);
-        out.push(nums[hare]);
+    for(let i = 0; i < nums.length;i+=2){
+        out[i] = nums[turtle];
+        out[i+1] = nums[hare];
         turtle++;
         hare++;
     }
