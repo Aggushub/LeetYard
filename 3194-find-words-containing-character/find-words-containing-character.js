37acb62967c05cd40091d44410efcedb07af412c
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-10-06 17:24:16 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (60.47%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -1,8 +1,3 @@
-/**
- * @param {string[]} words
- * @param {character} x
- * @return {number[]}
- */
 var findWordsContaining = function(words, x) {
     let out = [];
     for (let i = 0;i < words.length;i++){
