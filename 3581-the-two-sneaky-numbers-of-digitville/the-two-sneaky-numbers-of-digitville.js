daa9e34ae7aa848e15a06a341bc8ca9d86ea1862
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-11-02 23:45:09 +0530

    Time: 1 ms (86.63%) | Memory: 55.7 MB (85.05%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.js
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.js
@@ -1,18 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var getSneakyNumbers = function(nums) {
-    let hash = {};
-    let out = [];
-    for (let i = 0;i<nums.length;i++){
-        if(!hash[nums[i]]){
-            hash[nums[i]] = 1;
-        }
-        else{
-            hash[nums[i]]++;
-            out.push(nums[i]);
-        }
+    let town = {};
+    let sneakers = [];
+    for (each of nums){
+        if (each in town)
+        sneakers.push(each);
+        else
+        town[each] = 1;
     }
-    return out;
+    return sneakers;
+    
 };
