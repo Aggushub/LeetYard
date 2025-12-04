c9d3767716f06fe6d7e8c37c973200421c235354
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-12-04 18:25:08 +0530

    Time: 26 ms (6.70%) | Memory: 12.4 MB (80.41%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -1,17 +1,9 @@
-class Solution:
-    def possibleStringCount(self, word: str) -> int:
-        turtle = ""
-        hare = ""
+class Solution(object):
+    def possibleStringCount(self, word):
         output = 1
-        for i in range(0,len(word)-1):
-            turtle = word[i]
-            hare = word[i+1]
-            if turtle == hare:
-                output +=1
-
+        for i in range(1,len(word)):
+            if word[i] == word[i-1]:
+                output+=1
         return output
+
         
-                
-
-
-
