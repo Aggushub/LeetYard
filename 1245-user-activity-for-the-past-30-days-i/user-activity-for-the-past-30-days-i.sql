63734d46b6a6e9026b12828b531a143f727636d4
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-12-10 12:47:38 +0530

    Time: 342 ms (89.03%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,4 @@
-SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE activity_date > "2019-06-27" AND activity_date <= "2019-07-27"
+WHERE activity_date > '2019-06-27' and activity_date <= '2019-07-27'
 GROUP BY activity_date;
