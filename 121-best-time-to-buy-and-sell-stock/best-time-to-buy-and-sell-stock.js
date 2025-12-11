c4f6b5254a378ebe74ccbed427c66d1bed2d3d63
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-12-11 19:52:28 +0530

    Time: 5 ms (21.69%) | Memory: 66 MB (26.30%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -1,11 +1,11 @@
 var maxProfit = function(prices) {
     let profit = 0;
-    let left = 0
-    for (let right =1;right<prices.length;right++){
-        if (prices[left]>prices[right])
-        left = right;
-        else
-        profit = Math.max(profit,prices[right]-prices[left])
+    let min = prices[0]
+    for (let each of prices){
+        min = Math.min(min,each);
+        curr_profit = each - min;
+        profit = Math.max(curr_profit,profit)
     }
-    return profit
+    return profit;
+    
 };
