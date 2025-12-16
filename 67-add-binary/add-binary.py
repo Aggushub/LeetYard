fd55b8d30c3693989a5b2821775b64177c8eb614
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2025-12-16 10:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (74.73%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,9 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = int(a, 2)
-        b = int(b, 2)
-
+        a = int(a,2)
+        b = int(b,2)
         sumab = a + b
-        sum_bin = bin(sumab)
-        return sum_bin[2:]
+        sumab = bin(sumab)
+        return sumab[2:]
         
