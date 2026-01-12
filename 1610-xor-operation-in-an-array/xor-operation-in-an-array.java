e8adb74333b1f62bcaf245bd7b8d52988039a5b4
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2026-01-12 11:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (43.20%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int xorOperation(int n, int start) {  
+    int output = 0;
+    for (int i = 0; i < n; i++) {
+        output ^= (start + 2 * i);
+    }
+        return output;
+    }
+}
