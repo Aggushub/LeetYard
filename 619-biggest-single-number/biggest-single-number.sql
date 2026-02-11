0213bef2afe5d6816a38245dca67e74c56dd4aef
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2026-02-11 19:54:04 +0530

    Time: 556 ms (8.35%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,5 @@
-SELECT MAX(num) AS num
-FROM (
-    SELECT num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-) AS t;
+SELECT MAX(num) as num
+FROM (SELECT num
+FROM MyNumbers
+GROUP BY num
+HAVING COUNT(num) = 1) AS Q;
