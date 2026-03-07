97df260ada662e94f8e2bc48a0eb281c63acc680
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2026-03-07 21:03:56 +0530

    Time: 635 ms (88.23%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,5 +1,5 @@
-SELECT s.user_id, ROUND(COUNT(CASE WHEN c.action = 'confirmed' THEN 1 ELSE NULL END)*1.0/COUNT(s.user_id),2) as confirmation_rate
+SELECT s.user_id AS "user_id", ROUND(COUNT(CASE WHEN c.action = 'confirmed' THEN 1 ELSE NULL END)*1.0 / COUNT(s.user_id),2) AS "confirmation_rate"
 FROM Signups s
 LEFT JOIN Confirmations c
 ON s.user_id = c.user_id
-GROUP BY s.user_id; 
+GROUP BY s.user_id
