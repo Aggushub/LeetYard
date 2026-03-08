c01793ce511ad2e2765e9981e848f1d7398d37f5
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2026-03-08 21:09:41 +0530

    Time: 2680 ms (57.54%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,4 @@
-SELECT contest_id, ROUND((COUNT(contest_id) * 100.00 / (SELECT COUNT(user_id) FROM Users)),2) AS percentage
+SELECT contest_id, ROUND((COUNT(user_id)*100.00 / (SELECT COUNT(*) FROM Users)),2) AS percentage
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id ASC
