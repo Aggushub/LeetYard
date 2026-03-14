36a1a75c35816c3b09be69ff5f9a40f64da393c6
Joel Amos Philip <163115164+Aggushub@users.noreply.github.com>
2026-03-14 23:06:45 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (36.76%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,11 @@
+var climbStairs = function(n) {
+    if (n === 1)
+    return 1;
+    if (n === 2)
+    return 2;
+    let dp = [1,2]
+    for(let i = 2; i < n; i++){
+        dp.push(dp[i-1]+dp[i-2]);
+    }
+    return dp[n-1];
+};
